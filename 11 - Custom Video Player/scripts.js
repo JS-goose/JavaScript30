5ed63e9a507eaa2431edd2f3ac21d98078481590
Jonathan Sexton <joncsexton@.com>
2019-01-01 10:00:18 -0600

    established variables

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,8 @@
+// Variables
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressFilled = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipBtns = player.querySelectorAll('[data-skip]');
+const sliders = player.querySelectorAll('.player__slider');

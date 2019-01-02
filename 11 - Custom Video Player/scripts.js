9854d8cb01cf01907dc20859ec946dffd96a3ea9
Jonathan Sexton <joncsexton@.com>
2019-01-02 16:04:20 -0600

    added play/pause icon update functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,25 +1,27 @@
 // Variables
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressFilled = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipBtns = player.querySelectorAll('[data-skip]');
-const sliders = player.querySelectorAll('.player__slider');
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressFilled = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipBtns = player.querySelectorAll("[data-skip]");
+const sliders = player.querySelectorAll(".player__slider");
 
 //Actions
 function playVideo() {
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
+// This function updates the play/pause icon depending on video state
 function updatePlayButton() {
-    console.log(this);
+  this.paused ? toggle.textContent = "►" : toggle.textContent = "pause";
 }
 //Connect Event Listeners
-video.addEventListener('click', playVideo);
-video.addEventListener('play', updatePlayButton);
-toggle.addEventListener('click', playVideo);
+video.addEventListener("click", playVideo);
+video.addEventListener("play", updatePlayButton);
+video.addEventListener("pause", updatePlayButton);
+toggle.addEventListener("click", playVideo);

5d46f463ca6ca89d3cb276633dddb78c85a4e1c8
Jonathan Sexton <joncsexton@.com>
2019-01-02 16:45:24 -0600

    established skip button functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -18,10 +18,24 @@ function playVideo() {
 
 // This function updates the play/pause icon depending on video state
 function updatePlayButton() {
-  this.paused ? toggle.textContent = "►" : toggle.textContent = "pause";
+  this.paused ? (toggle.textContent = "►") : (toggle.textContent = "pause");
 }
+
+function skip() {
+  if(this.dataset.skip === "-10") {
+      console.log(video.currentTime)
+      video.currentTime -= 10
+  } 
+
+  if (this.dataset.skip === "25") {
+    console.log(video.currentTime)
+    video.currentTime += 25;
+  }
+}
+
 //Connect Event Listeners
 video.addEventListener("click", playVideo);
 video.addEventListener("play", updatePlayButton);
 video.addEventListener("pause", updatePlayButton);
 toggle.addEventListener("click", playVideo);
+skipBtns.forEach((item) => item.addEventListener("click", skip));
